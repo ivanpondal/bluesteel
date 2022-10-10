1af9fce96ce77bcc02ef722e28a9242260181aaf
Iván Pondal <ivan.pondal@gmail.com>
2022-10-10 19:47:55 -0300

    Initial Commit

---
+++ BLEScanner/BLEScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScannerApp.swift
+//  BLEScanner
+//
+//  Created by iván pondal on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BLEScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
