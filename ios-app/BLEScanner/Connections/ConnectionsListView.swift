48fbd0257aad0552e527265bf5fe3bd6a2002def
Iván Pondal <ivan.pondal@gmail.com>
2023-05-11 21:40:47 -0300

    Add selected device to test case

--- ios-app/BLEScanner/Connections/ConnectionsListView.swift
+++ ios-app/BLEScanner/Connections/ConnectionsListView.swift
@@ -10,18 +10,10 @@ import SwiftUI
 
 struct ConnectionsListView: View {
     var bluetoothRadio: BluetoothRadio
-    @State var connectedDevices: [Device] = []
-    @State private var selectedDevices: [UUID: Bool]
+    @State private var connectedDevices: [Device] = []
+    @State private var selectedDevices: [UUID: Bool] = [:]
     @State private var testCase: String = ""
     private let testCases = ["SR-OW-1"]
-
-    init(bluetoothRadio: BluetoothRadio, connectedDevices: [Device] = []) {
-        self.bluetoothRadio = bluetoothRadio
-        self.connectedDevices = connectedDevices
-        self.selectedDevices = connectedDevices.reduce(into: [UUID: Bool]()) {
-            $0[$1.id] = false
-        }
-    }
 
     var body: some View {
         NavigationStack {
@@ -43,7 +35,9 @@ struct ConnectionsListView: View {
                         }
                     }.pickerStyle(.menu)
                 }.fixedSize()
-                NavigationLink(destination: TestCaseView(activeTestCase: TestCase.sampleData.first!).navigationBarHidden(true)) {
+                NavigationLink(destination: TestCaseView(activeTestCase: TestCase(
+                    id: testCase, devices: connectedDevices.filter({selectedDevices[$0.id] == true}))
+                ).navigationBarHidden(true)) {
                     Button("Run", action: {}).allowsHitTesting(false)
                 }
             }
@@ -52,15 +46,19 @@ struct ConnectionsListView: View {
             .receive(on: RunLoop.main)
             .map({ peripheral in
                 return Device(id: peripheral.identifier,rssi: 0, name:peripheral.name, advertisedServices: [])
-            })){ connectedDevices.append($0) }
+            })){
+                connectedDevices.append($0)
+                selectedDevices[$0.id] = false
+            }
         .onReceive(bluetoothRadio.disconnectionEventSubject.receive(on: RunLoop.main)){ device in
                 connectedDevices.removeAll(where: {$0.id == device.identifier})
+                selectedDevices.removeValue(forKey: device.identifier)
             }
     }
 }
 
 struct ConnectionsListView_Previews: PreviewProvider {
     static var previews: some View {
-        ConnectionsListView(bluetoothRadio: BluetoothRadio(), connectedDevices: Device.sampleData)
+        ConnectionsListView(bluetoothRadio: BluetoothRadio())
     }
 }
