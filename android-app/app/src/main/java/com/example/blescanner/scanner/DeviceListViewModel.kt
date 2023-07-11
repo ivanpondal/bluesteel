9a2b86788c2a40bf94fca72013be761de2ccd2ca
Iván Pondal <ivan.pondal@gmail.com>
2023-07-11 20:07:12 -0300

    Add debouncing

--- android-app/app/src/main/java/com/example/blescanner/scanner/DeviceListViewModel.kt
+++ android-app/app/src/main/java/com/example/blescanner/scanner/DeviceListViewModel.kt
@@ -1,15 +1,17 @@
 package com.example.blescanner.scanner
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.blescanner.model.BluetoothDevice
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
+@OptIn(FlowPreview::class)
 class DeviceListViewModel(private val bluetoothScanner: BluetoothScanner) : ViewModel() {
 
     private val scannedDevices: MutableSet<BluetoothDevice> = mutableSetOf()
@@ -20,8 +22,7 @@ class DeviceListViewModel(private val bluetoothScanner: BluetoothScanner) : View
 
     init {
         viewModelScope.launch {
-            bluetoothScanner.scannedDeviceEvent.collect { bluetoothDevice ->
-                Log.d("asadf", "$bluetoothDevice")
+            bluetoothScanner.scannedDeviceEvent.debounce(1000).collect { bluetoothDevice ->
                 scannedDevices.remove(bluetoothDevice)
                 scannedDevices.add(bluetoothDevice)
 
