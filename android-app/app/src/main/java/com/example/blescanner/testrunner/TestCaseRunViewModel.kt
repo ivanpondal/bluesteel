a90aefa33567626e9214ad24fa0d1daac2bebde7
Iván Pondal <ivan.pondal@gmail.com>
2023-08-08 23:37:23 -0300

    Add sending of random bytes

--- android-app/app/src/main/java/com/example/blescanner/testrunner/TestCaseRunViewModel.kt
+++ android-app/app/src/main/java/com/example/blescanner/testrunner/TestCaseRunViewModel.kt
@@ -10,7 +10,7 @@ import com.example.blescanner.model.BluetoothSession.Companion.MAX_ATT_MTU
 import com.example.blescanner.scanner.repository.ConnectedDeviceRepository
 import com.example.blescanner.testrunner.model.TestCaseId
 import kotlinx.coroutines.launch
-import java.nio.charset.StandardCharsets.UTF_8
+import kotlin.random.Random
 
 class TestCaseRunViewModel(
     private val connectedDeviceRepository: ConnectedDeviceRepository,
@@ -33,16 +33,25 @@ class TestCaseRunViewModel(
             }
     }
 
+    private fun randomArray(size: Int): ByteArray {
+        val randomMessage = ByteArray(size)
+        Random.Default.nextBytes(randomMessage)
+        return randomMessage
+    }
+
     fun runTest() {
         val firstDevice = devices.first()
         val session = connectedDeviceRepository.getById(firstDevice)
         viewModelScope.launch {
             session.discoverServices()
-            val mtu = session.requestMtu(MAX_ATT_MTU)
-            Log.d("TestRunner", "$mtu")
-            session.writeWithResponse(
-                SERVICE_UUID, CHARACTERISTIC_UUID, "me perdonass??".toByteArray(UTF_8)
-            )
+            val mtu = session.requestMtu(MAX_ATT_MTU) - 3
+            repeat(100) {
+                val randomMessage = randomArray(mtu)
+                session.writeWithResponse(
+                    SERVICE_UUID, CHARACTERISTIC_UUID, randomMessage
+                )
+            }
+            Log.d("TestRunner", "Finished")
         }
     }
 }
