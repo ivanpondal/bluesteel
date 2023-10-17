7dba2f7d9abd225d5322bf0cfa60644aebdf9b45
Iván Pondal <ivan.pondal@gmail.com>
2023-10-17 20:38:35 -0300

    Add central background mode

--- ios-app/BLEScanner/Infra/BluetoothRadio.swift
+++ ios-app/BLEScanner/Infra/BluetoothRadio.swift
@@ -40,14 +40,14 @@ class BluetoothRadio : NSObject, CBPeripheralManagerDelegate {
         centralManager = CBCentralManager(delegate: self, queue: .global(qos: .utility), options: [CBCentralManagerOptionShowPowerAlertKey: true])
         peripheralManager = CBPeripheralManager(delegate: self, queue: .global(qos: .utility), options: [CBPeripheralManagerOptionShowPowerAlertKey: true])
     }
-    
+
     private func peripheral(withId peripheralUUID: UUID) throws -> CBPeripheral {
         guard let peripheral = centralManager?.retrievePeripherals(withIdentifiers: [peripheralUUID])[0] else {
             throw BluetoothError(message: "Could not find peripheral")
         }
         return peripheral
     }
-    
+
     func connect(toPeripheralWithId uuid: UUID) {
         if (!connectedPeripherals.contains(where: {$0.key == uuid})){
             do {
@@ -127,23 +127,23 @@ extension BluetoothRadio: CBCentralManagerDelegate {
         }
         stateSubject.value = central.state
     }
-    
+
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         //        print("id:", peripheral.identifier, "state:", peripheral.state.rawValue, "rssi:", RSSI, "name:", peripheral.name ?? "n/a")
-        
+
         // hide invalid rssi advertisements
         if RSSI.intValue < 0 {
             peripheralSubject.send(PeripheralAdvertisement(peripheral: peripheral, advertisements: advertisementData, rssi: RSSI.intValue))
         }
     }
-    
+
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         peripheral.delegate = self
 
         connectionEventSubject.send(peripheral)
         print("connected to ", peripheral)
     }
-    
+
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         print("failed to connect to ", peripheral, error ?? "unkown error")
         connectedPeripherals.removeValue(forKey: peripheral.identifier)
@@ -160,16 +160,16 @@ extension BluetoothRadio: CBPeripheralDelegate {
     // MARK: Peripheral server
     static let chracteristicUUID = CBUUID(string: "A5C46D55-280D-4B9E-8335-BCA4C0977BDB")
     static let serviceUUID = CBUUID(string: "FE4B1073-17BB-4982-955F-28702F277F19")
-    
+
     fileprivate func createService() -> CBMutableService {
         let characteristic = CBMutableCharacteristic(type: BluetoothRadio.chracteristicUUID, properties: [.writeWithoutResponse, .write], value: nil, permissions: [.writeable])
         let service = CBMutableService(type: BluetoothRadio.serviceUUID, primary: true)
-        
+
         service.characteristics = [characteristic]
-        
+
         return service
     }
-    
+
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch peripheral.state {
         case .poweredOn:
@@ -180,7 +180,7 @@ extension BluetoothRadio: CBPeripheralDelegate {
             print("other")
         }
     }
-    
+
     func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
         if let errorDescription = error?.localizedDescription {
             print("something went wrong adding peripheral service: ", errorDescription)
@@ -189,7 +189,7 @@ extension BluetoothRadio: CBPeripheralDelegate {
             peripheral.startAdvertising([CBAdvertisementDataLocalNameKey: UIDevice.current.name, CBAdvertisementDataServiceUUIDsKey: [BluetoothRadio.serviceUUID]])
         }
     }
-    
+
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
         if let errorDescription = error?.localizedDescription {
             print("something went wrong advertising peripheral service: ", errorDescription)
@@ -200,10 +200,10 @@ extension BluetoothRadio: CBPeripheralDelegate {
 
     func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
         let firstRequest = requests[0]
-        
+
         for request in requests {
             guard let data = request.value else { peripheral.respond(to: firstRequest, withResult: .invalidAttributeValueLength); return }
-            
+
             print("receive message from ", request.central, String(bytes: data, encoding: .ascii)!)
         }
         peripheral.respond(to: firstRequest, withResult: .success)
@@ -227,7 +227,7 @@ extension BluetoothRadio: CBPeripheralDelegate {
         }
         continuation.resume(returning: peripheral)
     }
-    
+
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if let error {
             print("characteristic discovery failed: ", error)
