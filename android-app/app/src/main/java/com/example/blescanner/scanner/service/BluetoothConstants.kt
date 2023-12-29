ed920d22794ee7c3821f1816d0f4c6a50e6a0448
Iván Pondal <ivan.pondal@gmail.com>
2023-12-29 10:54:47 -0300

    Add uuid type

--- android-app/app/src/main/java/com/example/blescanner/scanner/service/BluetoothConstants.kt
+++ android-app/app/src/main/java/com/example/blescanner/scanner/service/BluetoothConstants.kt
@@ -4,7 +4,7 @@ import com.example.blescanner.advertiser.GattService
 import java.util.UUID
 
 object BluetoothConstants {
-    val WRITE_SERVICE_UUID = UUID.fromString("7B442D4E-8D78-4214-97B3-3B2969709D69")
+    val WRITE_SERVICE_UUID: UUID = UUID.fromString("7B442D4E-8D78-4214-97B3-3B2969709D69")
     val WRITE_CHARACTERISTIC_UUID: UUID = UUID.fromString("D0C253CA-07A9-47B2-BB7A-F877A56BE43B")
 
     val writeAckServer = GattService(
